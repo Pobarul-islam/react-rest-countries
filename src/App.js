067a0c31b311d1244d49d76f2ab72bb32ecbfe7b
Pobarul-islam <pobarul3@gmail.com>
2022-03-18 20:06:24 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -24,8 +24,20 @@ function LoadCountries() {
   return (
     <div>
       <h1>Visiting Evey country of the world!!!</h1>
-      <h3>Available Countries: ${ countries.length}</h3>
+      <h3>Available Countries: {countries.length}</h3>
+       {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   );
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: { props.population}</h4>
+    </div>
+  )
 }
 export default App;
